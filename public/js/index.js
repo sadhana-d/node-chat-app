aa45285ad2de27929d0fe5d529a7e11a9c8dd494
Sadhana <sadhanads@rediffmail.com>
2018-10-01 11:05:30 -0500

    Event EMIT LISTENER

---
+++ public/js/index.js
@@ -0,0 +1,17 @@
+var socket = io();
+  socket.on('connect', function() {
+    console.log("Connected");
+  });
+
+  socket.emit('createMessage', {
+     to:'sun@gmail.com',
+     text: 'Have a nice day!'
+  });
+
+ socket.on('disconnect', function() {
+   console.log("Disconnected");
+ });
+
+ socket.on('newMessage', function(message){
+   console.log(message);
+ });
--- server/server.js
+++ server/server.js
@@ -1,15 +1,37 @@
 var path = require('path');
+var http = require('http');
 var express = require('express');
-var port = process.env.PORT || 3000;
-var app = express();
+var socketIO = require('socket.io');
 
 var publicPath = path.join(__dirname,"../public");
+var port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
-console.log(publicPath);
+
+io.on('connection', (socket) => {
+  console.log("new User connected");
+
+socket.emit('newMessage', {
+   from: 'sun@gmail.com',
+   text: 'Hey! How are you doing?',
+   createdAt: '12'
+});
+
+socket.on('createMessage', (message) => {
+  console.log(message);
+});
+
+  socket.on('disconnect', () => {
+    console.log("Disconnected");
+  });
+});
+
 // app.get('/', (req, res) => {
 //   res.render('index.html');
 // });
-app.listen(port,() => {
+server.listen(port,() => {
   console.log("Server started");
 });
